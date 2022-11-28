a57eaf3a67e6d9ac6fa08b2e3c22722808f17366
Alvin Mithamo <alvinmithamo100@gmail.com>
2022-11-28 13:28:22 +0300

    Scope lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = 'BOB';
+};
+
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavouriteCustomer = "sleek";
+function changeLeastFavouriteCustomer(){
+    return leastFavouriteCustomer = "Wilberforce";
+}
